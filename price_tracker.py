3964e9d6cfed8fcbb9c17aa729b438b840fe7cda
nagaokakid <nagaokakid@gmail.com>
2023-06-06 19:48:42 -0500

    Added options to web driver for headless window (no GUI) and figured out how to get html source code from a web page

--- price_tracker.py
+++ price_tracker.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# Enable headless mode for options, so browser window does not appear in GUI
+options = Options()
+options.add_argument("--headless=new")
+options.add_argument("--window-size=1920,1200")
+
+# Initialize web driver for Chrome
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+
+# Navigate to URL
 driver.get("https://www.python.org")
-time.sleep(5)
-driver.close()
+page_html = (driver.page_source).encode("utf-8", "ignore")
+print(page_html)
 
-class GenerateReport:
-    def __init__(self):
-        pass
-
-class AmazonAPI:
-    def __init__(self):
-        pass
+# Shut down web driver
+driver.quit()
